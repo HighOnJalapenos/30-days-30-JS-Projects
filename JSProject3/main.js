133038b0ece8ab533a8bdc463d8a95e063db4744
Digvijay Singh <dvsingh958@gmail.com>
2024-03-28 20:23:23 +0530

    project 3 used setProperty and check css webapi

---
+++ JSProject3/main.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate(e) {
+  const suffix = e.target.dataset.sizing || "";
+  const { name, value } = e.target;
+  document.documentElement.style.setProperty(`--${name}`, value + suffix);
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("input", handleUpdate));
